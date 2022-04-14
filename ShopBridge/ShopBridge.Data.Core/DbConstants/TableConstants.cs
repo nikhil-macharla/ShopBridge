05d41630b6c8a0a1a8731ff2fa6129de9433c25a
NikhilMacharla <nikhil1710macharla96@gmail.com>
2022-04-14 20:15:26 +0530

    Added Database migration

---
+++ ShopBridge/ShopBridge.Data.Core/DbConstants/TableConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopBridge.Data.Core.DbConstants
+{
+    /// <summary>
+    /// Table name constants
+    /// </summary>
+    public static class TableConstants
+    {
+        public static readonly string Item = "item";
+    }
+}
---
+++ ShopBridge/ShopBridge.Migrations/Program.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Reflection;
+
+namespace ShopBridge.Migrations
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(config => config.AddJsonFile("appsettings.json", false))
+                .ConfigureServices((hostContext, services) =>
+                {
+                    //services.AddHostedService<MigrationRunnerService>();
+                    services.AddFluentMigratorCore()
+                    .ConfigureRunner(runner =>
+                        runner.AddSqlServer()
+                        .WithGlobalConnectionString(hostContext.Configuration.GetConnectionString("ShopBridgeConnection"))
+                        .WithGlobalCommandTimeout(TimeSpan.FromMinutes(5))
+                        .ScanIn(Assembly.GetExecutingAssembly()).For.All()
+                    );
+                })
+                .ConfigureLogging(logging => logging.AddFluentMigratorConsole());
+
+            using var host = builder.Build();
+            var migrator = host.Services.GetService<IMigrationRunner>();
+            migrator.MigrateUp();
+        }
+    }
+}
